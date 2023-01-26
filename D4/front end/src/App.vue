7668b0e994c3416c9f36b418127ef0d82f14f051
TitanWasHere <andrea.gravili8@gmail.com>
2023-01-26 16:26:26 +0100

    prima fase D4 daje

---
+++ D4/front end/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <NavBar/>
+    <EarthHome/>
+    <IssAPI/>
+    <ComponentsView/>
+    <!--<HelloWorld msg="ISS4U"/>-->
+    
+    <MyFooter/>
+  </div>
+</template>
+
+<script>
+//import HelloWorld from './components/HelloWorld.vue'
+import NavBar from './components/NavBar.vue'
+import EarthHome from './components/EarthHome.vue'
+import IssAPI from './components/IssAPI.vue';
+import ComponentsView from './components/ComponentsView.vue';
+import MyFooter from './components/MyFooter.vue';
+
+
+export default {
+  name: 'App',
+  components: {
+    /*HelloWorld,*/ NavBar, EarthHome, IssAPI, ComponentsView, MyFooter
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
---
+++ D4/front end/src/components/ComponentsView.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="body_components">
+        <!-- 
+          -- Creazione di una search bar in alto a sinistra 
+          -- Scritta centrale in azzurro con underline centrale
+          -- Prendere dal DB ogni componente e visualizzarlo in una card
+          -- Footer
+        -->
+        
+    </div>
+</template>
+
+<script>
+
+
+    /*export default class myCard{
+        name = "";
+        preView = "";
+        fullDescription = "";
+        nation = "";
+        type_application = "";
+        operator = "";
+        contractors = "";
+        dimension_mass = "";
+        other_info = "";
+
+        constructor(name, preView, fullDescription, nation, type_application, operator, contractors, dimension_mass, other_info){
+            this.name = name;
+            this.preView = preView;
+            this.fullDescription = fullDescription;
+            this.nation = nation;
+            this.type_application = type_application;à
+            this.operator = operator;
+            this.contractors = contractors;
+            this.dimension_mass = dimension_mass;
+            this.other_info = other_info;
+        }
+
+        createCard(){
+            return '<div>'+this.name+'</div>';
+        }
+    }*/
+
+
+</script>
