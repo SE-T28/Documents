bf4b5bdae895fa803d203074eb33a20a7908ab5d
Matteo Parma <parmamatteo02@gmail.com>
2023-08-23 21:15:57 +0200

    little changes

--- D4/app/models/task.js
+++ D4/app/models/task.js
@@ -4,7 +4,7 @@ const Task = mongoose.model("task", new mongoose.Schema({
     "data_inizio" : {type: Date, required: true},
     "data_fine" : {type: Date, required: true},
     "nome" : {type: String, required: true},
-    "modulo" : {type: String, required: true},
+    "modulo" : {type: String, required: false},
     "descrizione" : {type: String, required: false},
     "completata" : {type: Boolean, required: true, default: false},
     "userId" : {type: mongoose.Schema.Types.ObjectId, ref: "Utente"}
